a518c4107720a92a59d31e61054db02da5f7f9b9
Archshinde <archanashinde9990@gmail.com>
2024-08-11 23:14:15 +0530

    Portfolio

---
+++ PortFolio-master/src/components/About.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I am Archana Shinde, a passionate Web Developer with a keen eye for the MERN
+          stack. With a background in IT, my skills are highly aligned with your requirements for an internship or full-time job.
+          I strive to create impactful and visually stunning software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          <strong>Degree:</strong> Bachelor of Science in Information Technology [2020].<br />
+          <strong>University:</strong> Mumbai University.<br />
+          <strong>Certification:</strong> Scaler Software Development Engineering(FullStack).<br />
+          <strong>Valid Till:</strong> No Expiry.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Experience as a Software Web Developer Intern at Eternosoft LLP, Chennai. Proficient in Programming Languages and Frameworks with good problem-solving skills, particularly in Data Structures and Algorithms (DSA).
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          Web Development Intern at Eternosoft LLP, Chennai - 600106, February 2024 - Present.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative IT solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ PortFolio-master/src/components/Experiance.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Experience() {
+  return (
+    <div
+      name="Experience"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-16"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">Experience</h1>
+        <p className="text-lg">Web Development Intern at Eternosoft LLP,Chennai-600106,February 2024 -Present.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Experience;
